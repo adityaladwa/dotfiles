e8a3a3e630a59d22264708d7b8c2d43ec17413a7
Aditya Ladwa <ladwa.aditya@gmail.com>
2018-09-30 20:56:55 +0530

    Added All configs

---
+++ zsh/env.zsh
@@ -0,0 +1,10 @@
+# If you come from bash you might have to change your $PATH.
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+export ZSH="/Users/adityaladwa/.oh-my-zsh"
+
+export GOPATH=$HOME/code/GoProjects
+export PATH=$GOPATH/bin:$PATH
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=/usr/local/mysql-8.0.12-macos10.13-x86_64/bin:$PATH
+export PATH=/Users/adityaladwa/Library/Android/sdk/platform-tools:$PATH
+export PATH=/Applications/Postgres.app/Contents/Versions/10/bin:$PATH
---
+++ zsh/zshrc.zsh
@@ -0,0 +1,18 @@
+# source ~/.zsh_plugins.sh # Load zsh plugins
+
+
+# source ~/.dotfiles/zsh/functions/functions.zsh
+# source ~/.dotfiles/zsh/functions/fzf-functions.zsh
+
+ZSH_THEME="robbyrussell"
+
+source ~/.dotfiles/zsh/functions/git-functions.zsh
+source ~/.dotfiles/zsh/env.zsh
+source ~/.dotfiles/zsh/options.zsh
+source ~/.dotfiles/zsh/alias.zsh
+
+autoload -U promptinit; promptinit
+prompt pure
+
+source <(antibody init)
+antibody bundle < ~/.zsh_plugins.txt
