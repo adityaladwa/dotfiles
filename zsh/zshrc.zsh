0dd8ac42687c9c68c765398925fd63cab02690ce
Aditya Ladwa <ladwa.aditya@gmail.com>
2019-05-16 10:22:40 +0530

    Removed extra alias file

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -7,6 +7,7 @@
 ZSH_THEME="robbyrussell"
 
 source ~/.dotfiles/zsh/functions/git-functions.zsh
+source ~/.dotfiles/zsh/functions/alias.zsh
 source ~/.dotfiles/zsh/env.zsh
 source ~/.dotfiles/zsh/options.zsh
 source ~/.dotfiles/zsh/alias.zsh
