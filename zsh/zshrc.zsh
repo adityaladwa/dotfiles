74703f50ee8bee491131e62982b0d3def100962c
Aditya Ladwa <ladwa.aditya@gmail.com>
2019-05-22 14:03:04 +0530

    Fix: Fixed pure prompt text on new terminal open

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -7,8 +7,5 @@ source ~/.dotfiles/zsh/options.zsh
 source ~/.dotfiles/zsh/alias.zsh
 source ~/.nix-profile/etc/profile.d/nix.sh
 
-autoload -U promptinit; promptinit
-prompt pure
-
 source <(antibody init)
 antibody bundle < ~/.dotfiles/zsh/zsh_plugins.txt
