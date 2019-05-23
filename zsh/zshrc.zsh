af12a73eb7b44ef7dc9e54cbf133c821117fad63
Aditya Ladwa <ladwa.aditya@gmail.com>
2019-05-23 15:34:06 +0530

    Added dotbot as dotfile bootstrapper

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -6,6 +6,6 @@ source ~/.dotfiles/zsh/env.zsh
 source ~/.dotfiles/zsh/options.zsh
 source ~/.dotfiles/zsh/alias.zsh
 source ~/.nix-profile/etc/profile.d/nix.sh
-
+source /run/current-system/sw/bin
 source <(antibody init)
 antibody bundle < ~/.dotfiles/zsh/zsh_plugins.txt
