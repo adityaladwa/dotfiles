c513aea93ca1bbaaf623732a97b8aad189d728c4
Aditya Ladwa <ladwa.aditya@gmail.com>
2019-06-25 18:16:57 +0530

    Removed unused source

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -5,7 +5,5 @@ source ~/.dotfiles/zsh/alias.zsh
 source ~/.dotfiles/zsh/env.zsh
 source ~/.dotfiles/zsh/options.zsh
 source ~/.dotfiles/zsh/alias.zsh
-source ~/.nix-profile/etc/profile.d/nix.sh
-source /run/current-system/sw/bin
 source <(antibody init)
 antibody bundle < ~/.dotfiles/zsh/zsh_plugins.txt
