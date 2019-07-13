dd0f245e50dc89464899fbe1a15435f3b9f3ba46
Aditya Ladwa <ladwa.aditya@gmail.com>
2019-07-13 12:29:11 +0530

    Fix: zsh

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,10 +1,14 @@
 ZSH_THEME="robbyrussell"
 
+export LC_ALL=en_US.UTF-8
 source ~/.dotfiles/zsh/functions/git-functions.zsh
+
 source ~/.dotfiles/zsh/alias.zsh
 source ~/.dotfiles/zsh/env.zsh
 source ~/.dotfiles/zsh/options.zsh
 source ~/.dotfiles/zsh/alias.zsh
+
 source <(antibody init)
 antibody bundle < ~/.dotfiles/zsh/zsh_plugins.txt
+
 source ~/.dotfiles/zsh/pure.zsh
