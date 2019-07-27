f4e55675a6d18d81bcdbbd1e2d84c6d0f6477ae7
Aditya Ladwa <ladwa.aditya@gmail.com>
2019-07-27 19:47:00 +0530

    Update

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -5,5 +5,5 @@ alias vim="nvim"
 # Git
 alias gs='git status'
 alias gd='git diff'
-alias glrd='git pull --rebase develop'
+alias glrd='git pull --rebase origin develop'
 alias gds="git diff --staged"
