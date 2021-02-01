352735456e7cac547e4222cc4ad20b64f7c262c9
Aditya Ladwa <ladwa.aditya@gmail.com>
2021-02-01 11:50:42 +0530

    Update

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -7,6 +7,7 @@ source ~/.dotfiles/zsh/alias.zsh
 source ~/.dotfiles/zsh/env.zsh
 source ~/.dotfiles/zsh/options.zsh
 source ~/.dotfiles/zsh/alias.zsh
+source ~/.dotfiles/work/work.zsh
 
 source <(antibody init)
 antibody bundle < ~/.dotfiles/zsh/zsh_plugins.txt
