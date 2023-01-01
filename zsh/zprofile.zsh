9d6be1cdddccdf70aaba84cac845a66bf23a5436
Aditya Ladwa <ladwa.aditya@gmail.com>
2023-01-01 14:21:38 +0530

    Update

--- zsh/zprofile.zsh
+++ zsh/zprofile.zsh
@@ -4,3 +4,5 @@ export PATH="$PATH:/Users/adityaladwa/.local/bin"
 
 # Created by `pipx` on 2021-09-11 15:54:09
 export PATH="$PATH:/Users/adityaladwa/Library/Python/3.9/bin"
+# Set PATH, MANPATH, etc., for Homebrew.
+eval "$(/opt/homebrew/bin/brew shellenv)"
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -2,17 +2,17 @@ ZSH_THEME="robbyrussell"
 
 export LC_ALL=en_US.UTF-8
 source ~/.dotfiles/zsh/functions/git-functions.zsh
-
 source ~/.dotfiles/zsh/alias.zsh
 source ~/.dotfiles/zsh/env.zsh
-source ~/.dotfiles/work/work.zsh
+# source ~/.dotfiles/work/work.zsh
 
 source <(antibody init)
 antibody bundle < ~/.dotfiles/zsh/zsh_plugins.txt
 
+fpath+=("$(brew --prefix)/share/zsh/site-functions")
 source ~/.dotfiles/zsh/pure.zsh
 
-source /Users/adityaladwa/.config/broot/launcher/bash/br
+# source /Users/adityaladwa/.config/broot/launcher/bash/br
 
 export NVM_DIR="$HOME/.nvm"
   [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
