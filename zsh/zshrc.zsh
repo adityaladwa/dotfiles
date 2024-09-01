c43443c745f31533aa25fd521cf6fe8f98ebc181
Aditya Ladwa <ladwa.aditya@gmail.com>
2024-09-01 21:49:32 +0530

    OrbStack and fvm added

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -19,6 +19,11 @@ export NVM_DIR="$HOME/.nvm"
 
 eval "$(pyenv init --path)"
 
+## [Completion]
+## Completion scripts setup. Remove the following line to uninstall
+[[ -f /Users/aditya/.dart-cli-completion/zsh-config.zsh ]] && . /Users/aditya/.dart-cli-completion/zsh-config.zsh || true
+## [/Completion]
+
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
 [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
